92f9b599c3757d79a7664f8a5125541b072e914a
Gagandeep Kundi <kundgagandeep@gmail.com>
2022-08-30 17:55:33 +0200

    config_update

--- src/google_play_sentiment/pipelines/data_upload/nodes.py
+++ src/google_play_sentiment/pipelines/data_upload/nodes.py
@@ -1,8 +1,18 @@
 from sqlalchemy import create_engine
 import pandas as pd
+from kedro.config import ConfigLoader
+from kedro.framework.project import settings
+
+CONF_SOURCE = "new_conf"
+
+conf_path = str("C:/Users/gagan/Python Projects/kedro/kedro-sentiment-analysis/conf")
+conf_loader = ConfigLoader(conf_source=conf_path, env="base")
+credentials = conf_loader.get("credentials*", "credentials*/**")
+
+creds = credentials['postgres']['con']
 
 def data_upload(df: pd.DataFrame):
     
-    engine = create_engine('postgresql://postgres:Speedy3861@localhost:5432/mydb')
+    engine = create_engine(creds)
     
     return df.to_sql('play_store', engine, if_exists='replace')
